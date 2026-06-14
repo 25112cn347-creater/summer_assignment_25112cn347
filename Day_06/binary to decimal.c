713a4ccf5a0e859d53b1ab49d9270d0c2fd57592
25112cn347-creater <25112cn347@glbitm.ac.in>
2026-06-14 17:16:53 +0530

    Create binary to decimal.c

---
+++ Day_06/binary to decimal.c
@@ -0,0 +1,18 @@
+ //Write a program to Convert binary to decimal. 
+    #include<stdio.h>
+    int main() {
+    int binary, rem, decimal = 0, base = 1;
+
+    printf("Enter a binary number: ");
+    scanf("%d", &binary);
+
+    while (binary != 0) {
+        rem = binary % 10;
+        decimal = decimal + rem * base;
+        base = base * 2;
+        binary = binary / 10;
+    }
+
+    printf("Decimal = %d", decimal);
+return 0;
+    }
