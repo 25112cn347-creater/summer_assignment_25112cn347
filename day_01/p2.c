4166a5725b742d1a4199974f3a66e33c0bbb79a5
25112cn347-creater <25112cn347@glbitm.ac.in>
2026-06-14 15:50:04 +0530

    Create p2.c

---
+++ day_01/p2.c
@@ -0,0 +1,12 @@
+// 2. wap to print a multiplication of table of a given number
+#include <stdio.h>
+int main{
+int n,i;
+char c;
+printf("the table of number:");
+scanf("%d",&n);
+char c=X;
+for(i=1;i<=10;i++)
+{printf("the table of %d %d%d",n,i,n*i);
+return 0 ;
+}
