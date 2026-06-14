c2959df792218df7cd4c7aff14a5c7b7699b7136
25112cn347-creater <25112cn347@glbitm.ac.in>
2026-06-14 15:48:27 +0530

    Create p1.c

---
+++ day_01/p1.c
@@ -0,0 +1,12 @@
+//Write a program to Calculate sum of first N natural numbers.
+#include<stdio.h>
+int main()
+{
+  int n,sum,i;
+printf("enter the number:");
+scanf("%d',&n);
+  for(i=1;i<=n;i+=)
+  { sum=sum+i}
+  printf("the sum of n natural numbers:",sum);
+return 0;
+}
