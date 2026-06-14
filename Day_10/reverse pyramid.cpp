1d1d243fb5859f57cafba41f4cffb0315590de7f
25112cn347-creater <25112cn347@glbitm.ac.in>
2026-06-14 18:11:49 +0530

    Create reverse pyramid.cpp

---
+++ Day_10/reverse pyramid.cpp
@@ -0,0 +1,27 @@
+/* Write a program to Print reverse pyramid. 
+   ******* 
+    *****   
+     ***   
+      *      */
+
+   #include <iostream>
+using namespace std;
+int main(){
+   int n;
+    cout<<"enter number of rows = ";
+    cin >> n;
+    int nst;
+    
+    nst = 2*n-1;
+    for (int i=1;i<=n;i++){
+        
+    for (int j=1;j<=i-1;j++){
+            cout<<" ";
+        }
+        for (int k=1;k<=nst;k++){
+            cout <<"*";
+        }
+        nst=nst-2;
+        cout<<endl;
+    }
+}
