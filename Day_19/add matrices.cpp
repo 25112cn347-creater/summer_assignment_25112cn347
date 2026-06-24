f2a14e307a4c750102664c2bf75a5539f51547eb
25112cn347-creater <25112cn347@glbitm.ac.in>
2026-06-24 21:38:11 +0530

    add matrices.cpp

--- Day_19/add matrices.cpp
+++ Day_19/add matrices.cpp
@@ -1,34 +1,35 @@
 // wap to add matrices.
-#include <iostream>
-#include <vector>
+
+   #include <iostream>
 using namespace std;
 
-vector<vector<int>> addMatrices(const vector<vector<int>>& A,
-                                const vector<vector<int>>& B) {
-    int rows = A.size();
-    int cols = A[0].size();
+int main() {
+    int r, c;
 
-    vector<vector<int>> result(rows, vector<int>(cols));
+    cout << "Enter rows and columns: ";
+    cin >> r >> c;
 
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            result[i][j] = A[i][j] + B[i][j];
+    int A[10][10], B[10][10];
+
+    cout << "Enter first matrix:\n";
+    for(int i = 0; i < r; i++) {
+        for(int j = 0; j < c; j++) {
+            cin >> A[i][j];
         }
     }
 
-    return result;
-}
+    cout << "Enter second matrix:\n";
+    for(int i = 0; i < r; i++) {
+        for(int j = 0; j < c; j++) {
+            cin >> B[i][j];
+        }
+    }
 
-int main() {
-    vector<vector<int>> A = {{1, 2}, {3, 4}};
-    vector<vector<int>> B = {{5, 6}, {7, 8}};
-
-    vector<vector<int>> sum = addMatrices(A, B);
-
-    cout << "Sum Matrix:\n";
-    for (const auto& row : sum) {
-        for (int x : row)
-            cout << x << " ";
+    cout << "addition Matrix:\n";
+    for(int i = 0; i < r; i++) {
+        for(int j = 0; j < c; j++) {
+            cout << A[i][j] + B[i][j] << " ";
+        }
         cout << endl;
     }
 
