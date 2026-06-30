3541c267c1057b5590be3ea686be85e19eddbd8e
25112cn347-creater <25112cn347@glbitm.ac.in>
2026-06-30 13:21:44 +0530

    Create compress a string.cpp

---
+++ Day_24/compress a string.cpp
@@ -0,0 +1,24 @@
+//Write a program to Compress a string. 
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+    
+    cout << "Enter a string: ";
+    getline(cin, s);
+
+    for (int i = 0; i < s.length(); i++) {
+        int count = 1;
+
+        // Count repeated characters
+        while (i < s.length() - 1 && s[i] == s[i + 1]) {
+            count++;
+            i++;
+        }
+
+        cout << s[i] << count;
+    }
+
+    return 0;
+}
