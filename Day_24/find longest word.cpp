06a87412a4b42ef727d0ca4c7255e03522c9cccb
25112cn347-creater <25112cn347@glbitm.ac.in>
2026-06-30 13:24:46 +0530

    Create find longest word.cpp

---
+++ Day_24/find longest word.cpp
@@ -0,0 +1,25 @@
+//Write a program to Find longest word. 
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str, word = "", longest = "";
+
+    cout << "Enter a sentence: ";
+    getline(cin, str);
+
+    for (int i = 0; i <= str.length(); i++) {
+        if (str[i] != ' ' && str[i] != '\0') {
+            word += str[i];
+        } else {
+            if (word.length() > longest.length()) {
+                longest = word;
+            }
+            word = "";
+        }
+    }
+
+    cout << "Longest word: " << longest;
+
+    return 0;
+}
