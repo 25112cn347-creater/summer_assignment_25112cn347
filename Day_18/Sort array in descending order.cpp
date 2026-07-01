7577fda5a9c44bbec44bea43563d87f413bdb901
25112cn347-creater <25112cn347@glbitm.ac.in>
2026-07-01 22:07:46 +0530

    Create  Sort array in descending order.cpp

---
+++ Day_18/Sort array in descending order.cpp
@@ -0,0 +1,33 @@
+//Write a program to Sort array in descending order. 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[100], n, i, j, temp;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    cout << "Enter array elements: ";
+    for(i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Sorting in descending order
+    for(i = 0; i < n - 1; i++) {
+        for(j = i + 1; j < n; j++) {
+            if(arr[i] < arr[j]) {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    cout << "Array in Descending Order: ";
+    for(i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
